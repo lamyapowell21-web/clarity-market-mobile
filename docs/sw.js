b35d1812722eafe2a52c67413e98a7b64b2dfbce
lamyapowell21-web <lamyapowell21@gmail.com>
2026-09-06 20:00:36 -0700

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,6 @@
+const CACHE="clarity-shell-v3";
+const SHELL=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{const url=new URL(e.request.url); if(url.origin!==self.location.origin) return;
+e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));});
